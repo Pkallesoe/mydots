82cc29b5d8e4510b6ed49df74e3f1a0cfa76a3c9
Peter S-K <pkallesoe@gmail.com>
2016-09-22 18:06:59 +0200

    I committed to git with my dot

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,11 @@
+import XMonad
+import XMonad.Config.Desktop
+
+baseConfig = desktopConfig
+
+main = xmonad baseConfig
+    { terminal    = "urxvt"
+    , modMask     = mod4Mask
+    , borderWidth = 1
+    }
+
